ab38b366e487fb483596716faa0f4ad170c8e097
WilliamWang <william.wang@flipos.cn>
2019-12-18 23:59:18 +0800

    1

--- Treasures/Collected/CollectedViewController.swift
+++ Treasures/Collected/CollectedViewController.swift
@@ -9,22 +9,42 @@
 import UIKit
 
 class CollectedViewController: UIViewController {
-
+    var dataSource = [TreasureListSearchDTO]()
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.setUI()
+        
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.initData()
     }
-    */
+    
+    func setUI() {
+    
+        tableView.register(TreasureListCellConstants.nib(), forCellReuseIdentifier: TreasureListCellConstants.reuseId)
+        tableView.tableFooterView = UIView()
+        tableView.rowHeight = TreasureListCellConstants.height
+        self.view.addSubview(tableView)
+    }
+    
+    func initData() {
+        dataSource = TreasureRepository.findCollectedTreasures()
+        self.tableView.reloadData()
+    }
+}
 
+extension CollectedViewController:UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TreasureListCellConstants.reuseId) as! TreasureListCell
+        cell.config(with: self.dataSource[indexPath.row])
+        return cell
+    }
 }
